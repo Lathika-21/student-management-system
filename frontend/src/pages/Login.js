8dd195cdb665b111868791434a0a14e25fec34c8
Lathika-21 <lathika2104@gmail.com>
2026-07-07 15:26:46 +0530

    Fix login navigation

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,9 +1,8 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import {toast} from "react-toastify";
 import { Link } from "react-router-dom";
 function Login() {
-    const navigate = useNavigate();
+    
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const login = async () => {
